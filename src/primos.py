71a0a8284f56e07f3c18fee006c7342d485766d6
Melina <yunoantognetti@gmail.com>
2024-03-27 17:08:34 -0300

    tercer commit

--- src/primos.py
+++ src/primos.py
@@ -1,10 +1,13 @@
+
+#hola, este es un comentario
+
 lower = 1
 upper = 100
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
+   
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:

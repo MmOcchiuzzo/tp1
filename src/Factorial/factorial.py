fac854346830148c994db315c2bca15050b97fe9
Melina <yunoantognetti@gmail.com>
2024-03-27 18:07:35 -0300

    cambio 3

--- src/Factorial/factorial.py
+++ src/Factorial/factorial.py
@@ -24,11 +24,23 @@ def calcular_factoriales(desde, hasta):
     for num in range(desde, hasta + 1):
         print("Factorial de", num, "! es", factorial(num))
 
+def obtener_rango(argumento):
+    if argumento.startswith("-"):
+        hasta = int(argumento[1:])
+        desde = 1
+    elif argumento.endswith("-"):
+        desde = int(argumento[:-1])
+        hasta = 60
+    else:
+        desde, hasta = map(int, argumento.split("-"))
+    return desde, hasta
+
 if len(sys.argv) == 1:
     rango = input("Por favor, ingrese el rango de números (desde-hasta): ")
-    desde, hasta = map(int, rango.split("-"))
 else:
-    desde, hasta = map(int, sys.argv[1].split("-"))
+    rango = sys.argv[1]
+
+desde, hasta = obtener_rango(rango)
 
 if desde >= hasta:
     print("El extremo 'desde' debe ser menor que el extremo 'hasta'.")
